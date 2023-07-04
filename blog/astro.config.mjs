42e1c8b836b8da8765d8fb8641c3845af2f025d2
Mark Topacio <mtopacio@illumina.com>
2023-07-04 03:53:13 -0700

    trying to commit to github

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  outDir: '../docs'
-  
+  outDir: '../docs',
+  site: 'https://mtopacio.github.io',
+  base: '/blog'
 });
