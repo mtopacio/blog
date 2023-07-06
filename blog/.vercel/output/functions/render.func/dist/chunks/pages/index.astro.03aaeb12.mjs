80809ca0133ec4accaafcf703fedfd68e3785357
Mark Topacio <mtopacio@illumina.com>
2023-07-06 02:07:36 -0700

    added vercel integration

---
+++ blog/.vercel/output/functions/render.func/dist/chunks/pages/index.astro.03aaeb12.mjs
@@ -0,0 +1,28 @@
+/* empty css                           */import { c as createAstro, a as createComponent, r as renderTemplate, d as renderComponent, m as maybeRenderHead } from '../astro.bc1e62c4.mjs';
+import 'html-escaper';
+import { $ as $$DefaultLayout } from './2023-07-04-project-announcement.md.6d34f753.mjs';
+import 'cookie';
+import 'kleur/colors';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+
+const $$Astro = createAstro("https://mtopacio.github.io");
+const $$Index = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Index;
+  return renderTemplate`<!-- Landing Page -->${renderComponent($$result, "DefaultLayout", $$DefaultLayout, { "title": "Mark Topacio" }, { "default": ($$result2) => renderTemplate`
+	${maybeRenderHead($$result2)}<div>
+		<div class="flex flex-row h-full w-full justify-center pt-10">
+			Hi! My name is Mark, and I work as a Process Engineer at Illumina focused on manufacturing.
+		</div>
+		<div id="background-plot"></div>
+	</div>
+	
+` })}`;
+}, "/home/mtopacio/Git/blog/blog/src/pages/index.astro");
+
+const $$file = "/home/mtopacio/Git/blog/blog/src/pages/index.astro";
+const $$url = "/blog";
+
+export { $$Index as default, $$file as file, $$url as url };
---
+++ blog/.vercel/output/functions/render.func/dist/chunks/posts@_@astro.2f31c16b.mjs
@@ -0,0 +1,13 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.bc1e62c4.mjs';
+import 'html-escaper';
+import 'cookie';
+import 'kleur/colors';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+
+const page = () => import('./pages/posts.astro.999f4e5c.mjs');
+
+export { page };
